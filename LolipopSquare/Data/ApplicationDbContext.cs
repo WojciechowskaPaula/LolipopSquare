3a3b4c356e9894bf6a33d4a85c478120bc4c10a5
Paula Wojciechowska <paulla.wojciechowska@gmail.com>
2022-07-10 14:03:54 +0200

    Adding connection string and DbContext configuration

---
+++ LolipopSquare/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LolipopSquare.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options){}
+    }
+}
--- LolipopSquare/Program.cs
+++ LolipopSquare/Program.cs
@@ -1,10 +1,16 @@
+using LolipopSquare.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
+var connectionString = builder.Configuration.GetConnectionString("defaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer(connectionString));
 
+var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
