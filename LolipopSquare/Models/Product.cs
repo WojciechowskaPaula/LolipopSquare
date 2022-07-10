136871b71535c9ef9620ebbceb9aea6636fdc59c
Paula Wojciechowska <paulla.wojciechowska@gmail.com>
2022-07-10 14:26:47 +0200

    Adding Product model

---
+++ LolipopSquare/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LolipopSquare.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Range(1,100000)]
+        public decimal Price { get; set; }
+
+        [Display(Name ="Product description")]
+        [MinLength(10, ErrorMessage ="Description must contains minimum 10 characters")]
+        public string Description { get; set; }
+        public bool Availability { get; set; }
+        public string Image { get; set; }
+        
+    }
+}
