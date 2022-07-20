674ab5ab1f2aeb133e85d5ea2fcba84a7f3a1ddd
Paula Wojciechowska <paulla.wojciechowska@gmail.com>
2022-07-20 11:12:15 +0200

    Adding DeleteProductVM

---
+++ LolipopSquare/Models/ViewModels/DeleteProductVM.cs
@@ -0,0 +1,10 @@
+﻿namespace LolipopSquare.Models.ViewModels
+{
+    public class DeleteProductVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+    }
+}
