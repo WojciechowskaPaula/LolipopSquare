7c9a9c754ddca6b3a2c0ade58fb9201ac7bad318
Paula Wojciechowska <paulla.wojciechowska@gmail.com>
2022-07-21 13:27:41 +0200

    Create EditCategoryVM

---
+++ LolipopSquare/Models/ViewModels/EditCategoryVM.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LolipopSquare.Models.ViewModels
+{
+    public class EditCategoryVM
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [Range(1, 30)]
+        public string Name { get; set; }
+    }
+}
