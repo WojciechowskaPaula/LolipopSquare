f74fe07e0e7add21161fb275d4d99313366b7ef9
Paula Wojciechowska <paulla.wojciechowska@gmail.com>
2022-08-09 13:44:51 +0200

    Creating ShoppingCartItem model

---
+++ LolipopSquare/Models/DTO/ShoppingCartItem.cs
@@ -0,0 +1,10 @@
+﻿namespace LolipopSquare.Models.DTO
+{
+    public class ShoppingCartItem
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
