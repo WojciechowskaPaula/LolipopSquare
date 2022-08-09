558098ec673814a15d78f6aa2bc64485a6d34856
Paula Wojciechowska <paulla.wojciechowska@gmail.com>
2022-08-09 15:57:24 +0200

    Creating a method to add an item to the shopping cart

---
+++ LolipopSquare/Controllers/ShoppingCartController.cs
@@ -0,0 +1,26 @@
+﻿using LolipopSquare.Interface;
+using LolipopSquare.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace LolipopSquare.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        private readonly IShoppingCartService _shoppingCartService;
+        public ShoppingCartController(IShoppingCartService shoppingCartService)
+        {
+            _shoppingCartService = shoppingCartService;
+        }
+        [HttpGet]
+        public IActionResult AddItem(int productId)
+        {
+            List<ShoppingCartItem> listOfItems = new List<ShoppingCartItem>();
+            var product = _shoppingCartService.GetProduct(productId);
+            listOfItems.Add(product);
+            string serializeList = JsonSerializer.Serialize(listOfItems);
+            HttpContext.Session.SetString("product", serializeList);
+            return View();
+        }
+    }
+}
