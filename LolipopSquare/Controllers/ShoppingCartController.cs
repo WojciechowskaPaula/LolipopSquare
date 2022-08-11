19038817cffdc7e3a3d3198610418f4b70ddec2a
Paula Wojciechowska <paulla.wojciechowska@gmail.com>
2022-08-11 14:24:26 +0200

    Adding the ability to add more than one product to the shopping cart

--- LolipopSquare/Controllers/ShoppingCartController.cs
+++ LolipopSquare/Controllers/ShoppingCartController.cs
@@ -16,11 +16,43 @@ namespace LolipopSquare.Controllers
         public IActionResult AddItem(int productId)
         {
             List<ShoppingCartItem> listOfItems = new List<ShoppingCartItem>();
-            var product = _shoppingCartService.GetProduct(productId);
-            listOfItems.Add(product);
+            var products = HttpContext.Session.GetString("product");
+            if(products != null)
+            {
+                var newProduct = _shoppingCartService.GetProduct(productId);
+               listOfItems = JsonSerializer.Deserialize<List<ShoppingCartItem>>(products);
+               listOfItems.Add(newProduct);
+
+            }
+            else
+            {
+                var product = _shoppingCartService.GetProduct(productId);
+                listOfItems.Add(product);
+            }
+            
+            
             string serializeList = JsonSerializer.Serialize(listOfItems);
             HttpContext.Session.SetString("product", serializeList);
-            return View();
+            return RedirectToAction("Index", "Product");
+        }
+
+        [HttpGet]
+        public IActionResult GetShoppingCartItems()
+        {
+
+            var products = HttpContext.Session.GetString("product");
+            if(products == null)
+            {
+                List<ShoppingCartItem> shoppingCartItems = new List<ShoppingCartItem>();
+                return View (shoppingCartItems);
+            }
+            else
+            {
+                var shoppingCartItems = JsonSerializer.Deserialize<List<ShoppingCartItem>>(products);
+                return View(shoppingCartItems);
+            }
+
+           
         }
     }
 }
