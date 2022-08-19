072d07e56438001adfa5e1b8438e30b97faa6fd9
Paula Wojciechowska <paulla.wojciechowska@gmail.com>
2022-08-19 15:12:42 +0200

    Adding AllProductsVM

---
+++ LolipopSquare/Models/ViewModels/AllProductsVM.cs
@@ -0,0 +1,11 @@
+﻿namespace LolipopSquare.Models.ViewModels
+{
+    public class AllProductsVM
+    {
+        public List<ProductVM> Products { get; set; }
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+        public string Search { get; set; }
+        public int Count { get; set; }
+    }
+}
