2e5a37d1527d73837722ee5bb87ef5734e228be4
Paula Wojciechowska <paulla.wojciechowska@gmail.com>
2022-09-26 14:36:08 +0200

    Remove unused properties from OrderSummaryVM

--- LolipopSquare/Models/ViewModels/OrderSummaryVM.cs
+++ LolipopSquare/Models/ViewModels/OrderSummaryVM.cs
@@ -1,13 +1,12 @@
 ﻿using LolipopSquare.Models.DTO;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace LolipopSquare.Models.ViewModels
 {
     public class OrderSummaryVM
     {
-        public int ProductId { get; set; }
-        public string ProductName { get; set; }
-        public decimal Price { get; set; }
-        public int Quantity { get; set; }
+        
         public int OrderId { get; set; }
         public DateTime DateOfOrder { get; set; }
         public decimal TotalPrice { get; set; }
