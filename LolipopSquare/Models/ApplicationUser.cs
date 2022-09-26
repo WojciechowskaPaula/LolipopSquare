9a221e8721f9e482e1427f222b0370aa02988eb9
Paula Wojciechowska <paulla.wojciechowska@gmail.com>
2022-09-26 14:45:55 +0200

    Adding display attribute in ApplicationUser model

--- LolipopSquare/Models/ApplicationUser.cs
+++ LolipopSquare/Models/ApplicationUser.cs
@@ -1,15 +1,27 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace LolipopSquare.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        [Display(Name ="First Name")]
         public string FirstName { get; set; }
+
+        [Display(Name ="Last Name")]
         public string LastName { get; set; }
+
+
+        [Display(Name ="Building number")]
         public string BuildingNo { get; set; }
+       
         public string Street { get; set; }
         public string City { get; set; }
+
+        [Display(Name ="Zip code")]
         public string ZipCode { get; set; }
+
+        [Display(Name ="Phone number")]
         public string PhoneNumber { get; set; }
         public ICollection<Order> Orders { get; set; }
     }
