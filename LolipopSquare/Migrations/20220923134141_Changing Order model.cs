b29710871e151b1d4babbe21f5bd0c3a5d2328da
Paula Wojciechowska <paulla.wojciechowska@gmail.com>
2022-09-26 14:30:22 +0200

    Adding OrderConfirmation to Order model

---
+++ LolipopSquare/Migrations/20220923134141_Changing Order model.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LolipopSquare.Migrations
+{
+    public partial class ChangingOrdermodel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "OrderConfirmation",
+                table: "Orders",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OrderConfirmation",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Quantity",
+                table: "Orders",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
